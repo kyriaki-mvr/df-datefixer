5c36ea8b9f10b9b4a35a7c29a860a63b7fc98631
kyriaki-mvr <kiriakimaur@yahoo.gr>
2025-05-24 13:09:04 +0300

    update readme

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="df-datefixer",
-    version="1.0.0",
-    description="Simple package to automatically standardize dates in Pandas DataFrames.",
+    version="1.0.1",
+    description="A package to automatically standardize dates in Pandas DataFrames.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kyriaki-mvr/df-datefixer",
     author="Kyriaki Mavrooulou",
     author_email="kyriaki@quanterra.gr",
-    packages=find_packages(where="src"),
+    packages=find_packages(),
     package_dir={'': 'src'},
     install_requires=[
         'pandas',
